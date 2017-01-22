e85aa3fe644155595c6e3c53cc5ddfbe25f22087
Abhinav Kumar <abhinavkumar.sieger@gmail.com>
2017-01-22 13:07:56 +0530

    Client Script is almost working.
    Few data packets are lost.
    Might be the problem of time synchronization between Arduino and Client
    Script.

---
+++ pyhonClientScript/testclient.py
@@ -0,0 +1,59 @@
+import socket   
+import sys  
+import struct
+import time
+
+#main function
+if __name__ == "__main__":
+
+    if(len(sys.argv) < 2) :
+        print 'Usage : python client.py hostname'
+        sys.exit()
+
+    host = sys.argv[1]
+    port = 8080
+
+#create an INET, STREAMing socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+
+print 'Socket Created'
+
+try:
+    remote_ip = socket.gethostbyname( host )
+    s.connect((host, port))
+
+except socket.gaierror:
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+
+#Send some data to remote server
+message = "Test"
+
+try :
+    #Set the whole string
+    while True:
+        try:
+            print "Reconnecting"
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            remote_ip = socket.gethostbyname( host )
+            s.connect((host, port))
+        except socket.gaierror:
+            print 'Hostname could not be resolved. Exiting'
+            sys.exit()
+        s.send(message)
+        time.sleep(2)
+        k=s.recv(15)
+        print "Got Data:",k
+        time.sleep(2)
+        
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+s.close()
